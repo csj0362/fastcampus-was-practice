4b5c53090269f5097f1e422d0ba680a4a015d7d4
csj <koreaxman@naver.com>
2022-10-06 16:38:49 +0900

    servlet

---
+++ src/main/java/org/example/CalculatorServlet.java
@@ -0,0 +1,35 @@
+package org.example;
+import org.example.calculator.Calculator;
+import org.example.calculator.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calculate")
+public class CalculatorServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+            log.info("service");
+
+            int operand1 = Integer.parseInt(req.getParameter("operand1"));
+            String operator = req.getParameter("operator");
+            int operand2 = Integer.parseInt(req.getParameter("operand2"));
+
+            int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+            PrintWriter writer = res.getWriter();
+            writer.println(result);
+
+    }
+}
